660ab6e2d03f5fe03706c46da42a4b118093e865
alexthehaszard <aggis.haszard@gmail.com>
2020-06-26 15:24:08 +1200

    forgot to add other files to commit

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let playCards = [];
 let currentCard;
 let currentCard2;
 let won;
+let usingBot = false;
+let wins = 0;
+let losses = 0;
+let games = 0;
+let lost;
 setup();
 shuffle();
 
@@ -15,7 +20,14 @@ function setup() {
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
   ];
+  shuffle();
   won = false;
+  lost = false;
+  document.getElementById("addCard").style = "";
+  for (let i = 0; i < playCards.length; i++) {
+    document.getElementById("cards").removeChild(playCards[i].element);
+  }
+  playCards = [];
 }
 
 function shuffle() {
@@ -27,7 +39,6 @@ function shuffle() {
       deck[i][ran] = temp;
     }
   }
-  console.log(deck);
 }
 
 function placeCard() {
@@ -36,123 +47,73 @@ function placeCard() {
     if (deck[suit].length === 0) {
       placeCard();
     } else {
-      playCards.push(new PlayCard(suit, deck[suit][0]));
-      playCards[playCards.length - 1].makeElement();
-      deck[suit].splice(0, 1);
-    }
-  }
-}
-
-class PlayCard {
-  constructor(suit, num) {
-    this.suit = suit;
-    this.num = num;
-    this.index = playCards.length;
-    this.element;
-    this.suitText;
-    this.numText;
-  }
-
-  makeElement() {
-    if (playCards.length <= 9) {
-      this.element = document.createElement("div");
-      this.suitText = document.createElement("p");
-      this.numText = document.createElement("p");
-      this.element.appendChild(this.numText);
-      this.element.appendChild(this.suitText);
-      this.element.classList = "card";
-      this.element.setAttribute(
-        "onclick",
-        `playCards[${this.index}].cardClicked()`
-      );
-      this.suitText.classList = "card-suit";
-      this.numText.classList = "card-num";
-      this.suitText.innerHTML = suits[this.suit];
-      this.numText.innerHTML = nums[this.num - 1];
-      document.getElementById("cards").appendChild(this.element);
-    }
-  }
-
-  cardClicked() {
-    if (!currentCard) {
-      this.element.style = "background-color: lightblue";
-      currentCard = playCards[this.index];
-    } else {
-      if (currentCard.num + this.num === 11) {
-        this.element.style = "background-color: lightgreen";
-        currentCard.element.style = "background-color: lightgreen";
-        setTimeout(() => {
-          this.element.style = "";
-          currentCard.element.style = "";
-          this.updateElement();
-          currentCard.updateElement();
-          currentCard = null;
-        }, 100);
-      } else if (currentCard.num > 10 && this.num > 10 && !currentCard2) {
-        currentCard2 = playCards[this.index];
-        currentCard2.element.style = "background-color: lightblue";
-      } else if (
-        currentCard2 &&
-        currentCard.num + currentCard2.num + this.num === 36
-      ) {
-        this.element.style = "background-color: lightgreen";
-        currentCard.element.style = "background-color: lightgreen";
-        currentCard2.element.style = "background-color: lightgreen";
-        setTimeout(() => {
-          this.element.style = "";
-          currentCard.element.style = "";
-          currentCard2.element.style = "";
-          this.updateElement();
-          currentCard.updateElement();
-          currentCard2.updateElement();
-          currentCard = null;
-          currentCard2 = null;
-        }, 100);
-      } else {
-        this.element.style = "background-color: lightcoral";
-        currentCard.element.style = "background-color: lightcoral";
-        if (currentCard2) {
-          currentCard2.element.style = "background-color: lightcoral";
-        }
-        setTimeout(() => {
-          this.element.style = "";
-          currentCard.element.style = "";
-          if (currentCard2) {
-            currentCard2.element.style = "";
-          }
-          currentCard = null;
-          currentCard2 = null;
-        }, 100);
-      }
-    }
-  }
-
-  updateElement() {
-    checkWin();
-    if (won === false) {
-      let suit = Math.floor(Math.random() * 4);
-      if (deck[suit].length === 0) {
-        this.updateElement();
-      } else {
-        this.num = deck[suit][0];
-        this.suit = suits[suit];
-        this.numText.innerHTML = nums[this.num - 1];
-        this.suitText.innerHTML = this.suit;
+      if(playCards.length < 8) {
+        playCards.push(new PlayCard(suit, deck[suit][0]));
+        playCards[playCards.length - 1].makeElement();
         deck[suit].splice(0, 1);
+      } else if (playCards.length === 8) {
+        playCards.push(new PlayCard(suit, deck[suit][0]));
+        playCards[playCards.length - 1].makeElement();
+        deck[suit].splice(0, 1);
+        document.getElementById("addCard").style = "display: none";
       }
     }
+    checkLoss();
   }
 }
+
 
 function checkWin() {
   let total = 0;
   for (let i = 0; i < deck.length; i++) {
     total += deck[i].length;
   }
-  if (total === 0 && won === false) {
-    document.getElementById("winText").style = "";
+  if (total === 0 && won === false && lost === false) {
     won = true;
-    document.getElementById("cards").innerHTML = "";
-    setup();
+    wins++;
+    games++;
+    document.getElementById("winLoss").innerHTML = `win/loss: ${wins/losses}`;
+    console.log("games: " + games);
+    if (usingBot === true) {
+      setTimeout(() => {
+        setup();
+        bot();
+      }, 100);
+    }
+    return true;
   }
+}
+
+function checkLoss() {
+  let isLost = true;
+  for (let i = 0; i < playCards.length; i++) {
+    for (let j = playCards.length-1; j > i; j--) {
+      if (playCards[i].num + playCards[j].num === 11) {
+        isLost = false;
+        return false;
+      } else if (playCards[i].num > 10 && playCards[j].num > 10 && playCards[j].num != playCards[i].num) {
+        for (let k = 0; k < playCards.length; k++) {
+          if (playCards[i].num + playCards[j].num + playCards[k].num === 36 && playCards[k].num > 10) {
+            isLost = false;
+            return false;
+          }
+        }
+      }
+    }
+  }
+  if (playCards.length === 9 && isLost === true && lost === false) {
+    lost = true;
+    losses++;
+    games++;
+    document.getElementById("winLoss").innerHTML = `win/loss: ${wins/losses}`;
+    console.log("games: " + games);
+    if (usingBot === true) {
+      setTimeout(() => {
+        setup();
+        bot();
+      }, 100);
+    }
+    return true;
+  }
+  return false;
 }
