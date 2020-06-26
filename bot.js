4d2fe78c163f86fe40b8d9d8b10a330a59676b67
alexthehaszard <aggis.haszard@gmail.com>
2020-06-26 15:22:30 +1200

    added bot

---
+++ bot.js
@@ -0,0 +1,35 @@
+function bot() {
+  checkLoss();
+  checkWin();
+  if (won === false && lost === false) {
+    for (let i = 0; i < playCards.length; i++) {
+      for (let j = i+1; j < playCards.length; j++) {
+        if (playCards[i].num + playCards[j].num === 11) {
+          playCards[i].cardClicked();
+          playCards[j].cardClicked();
+          setTimeout(() => {
+            bot();
+          }, 100);
+          return;
+        } else if (playCards[i].num > 10 && playCards[j].num > 10 && playCards[i].num !== playCards[j].num) {
+          for (let k = j; k < playCards.length; k++) {
+            if (playCards[j].num + playCards[i].num + playCards[k].num === 36 && playCards[k].num > 10) {
+              playCards[i].cardClicked();
+              playCards[j].cardClicked();
+              playCards[k].cardClicked();
+              setTimeout(() => {
+                bot();
+              }, 100);
+              return;
+            }
+          }
+        }
+      }
+    }
+    placeCard();
+    setTimeout(() => {
+      bot();
+    }, 100);
+    return;
+  }
+}
